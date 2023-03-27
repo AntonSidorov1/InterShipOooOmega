97819edd97438248534cf5ddc90f186173ff31db
AntonSidorov <g493-14-sidorov@git.vm>
2023-03-27 16:58:20 +0300

    Теперь можно фильтровать котиков и их модели по цвету, полу и породе

--- CatsShop/Controllers/ControllersThisWork/CatsController.cs
+++ CatsShop/Controllers/ControllersThisWork/CatsController.cs
@@ -32,15 +32,86 @@ namespace CatsShop.Controllers.ControllersThisWork
         }
 
         /// <summary>
+        /// Получить список котиков по ID цвета
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("By-Color/{id}")]
+        public List<CatWithModel> ByColor(int id)
+        {
+            return GetList().FindAll(model => model.Model.ColorID == id);
+        }
+
+        /// <summary>
+        /// Получить список котиков по ID породы
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("By-Species/{id}")]
+        public List<CatWithModel> BySpecies(int id)
+        {
+            return GetList().FindAll(model => model.Model.SpeciesID == id);
+        }
+
+        /// <summary>
+        /// Получить список котиков по ID пола
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("By-Gender/{id}")]
+        public List<CatWithModel> ByGender(int id)
+        {
+            return GetList().FindAll(model => model.Model.GenderID == id);
+        }
+
+        /// <summary>
+        /// Получить список котиков по ID модели
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("By-Model/{id}")]
+        public List<CatWithModel> ByModel(int id)
+        {
+            return GetList().FindAll(model => model.Model.ID == id);
+        }
+
+        /// <summary>
         /// Получить котика по его ID
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet("{id}")]
-        public CatWithModel? Get(int id)
+        public ActionResult<CatWithModel?> Get(int id)
         {
-            return GetList().FirstOrDefault(cat => cat.ID == id);
+            try
+            {
+                return Ok(GetList().FirstOrDefault(cat => cat.ID == id));
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
+
+        /// <summary>
+        /// Получить котика в позиции по её ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("By-Pozition/{id}")]
+        public ActionResult<CatWithModel?> ByPozition(int id)
+        {
+            try
+            {
+                int catID = PozitionsList.GetPositionsListFromDB().GetCatFromPozition(id).ID;
+                return Get(catID);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
 
         /// <summary>
         /// Добавить котика
@@ -91,25 +162,25 @@ namespace CatsShop.Controllers.ControllersThisWork
             return CatsList.GetCatsListFromDB().DeleteCat(id, session);
         }
 
-        /// <summary>
-        /// Получить список позиций котика
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet("{id}/Positions")]
-        public List<PozitionWithDates> GetPositions(int id) => PozitionsList.GetPositionsListFromDB().GetPositionForCat(id);
+        ///// <summary>
+        ///// Получить список позиций котика
+        ///// </summary>
+        ///// <param name="id"></param>
+        ///// <returns></returns>
+        //[HttpGet("{id}/Positions")]
+        //public List<PozitionWithDates> GetPositions(int id) => PozitionsList.GetPositionsListFromDB().GetPositionForCat(id);
 
-        /// <summary>
-        /// Получить модель котика
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        [HttpGet("{id}/Model")]
-        public CatModelFullDatas GetModelFromCat(int id)
-        {
-            int idCat = CatsList.GetCatsListFromDB().GetCatFromID(id).ModelID;
-            return CatModelFullDatas.GetModel(CatModelList.GetModelsListFromDB().GetModelFromID(idCat));
-        }
+        ///// <summary>
+        ///// Получить модель котика
+        ///// </summary>
+        ///// <param name="id"></param>
+        ///// <returns></returns>
+        //[HttpGet("{id}/Model")]
+        //public CatModelFullDatas GetModelFromCat(int id)
+        //{
+        //    int idCat = CatsList.GetCatsListFromDB().GetCatFromID(id).ModelID;
+        //    return CatModelFullDatas.GetModel(CatModelList.GetModelsListFromDB().GetModelFromID(idCat));
+        //}
 
 
     }
