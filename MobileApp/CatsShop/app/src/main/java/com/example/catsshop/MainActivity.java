65804d5844ebccf6777a95ba96bd1cc8df008715
AntonSidorov <g493-14-sidorov@git.vm>
2023-03-30 16:02:06 +0300

    Добавлена иконка к разрабатываемому мобильному приложению

--- MobileApp/CatsShop/app/src/main/java/com/example/catsshop/MainActivity.java
+++ MobileApp/CatsShop/app/src/main/java/com/example/catsshop/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.catsshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.DB.Helper;
@@ -29,4 +32,22 @@ public class MainActivity extends AppCompatActivity {
     {
         url.setText(Helper.GetUrlAddress(GetContext()));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_manu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.exitItem)
+        {
+            finish();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
 }
